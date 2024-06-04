80e17d9d77a3e67964cea187eb8b403821223832
tempDev01 <nectdev01@gmail.com>
2024-06-04 16:13:15 +0530

    Configured Prettier and Husky

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nextjs-client
+
 This repository contains the frontend code for our application, built with Next.js. It includes all the client-side components, pages, and assets necessary for the user interface.
--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
-    return (
-        <main className={styles.main}>
-            <div>Nectworks Migration</div>
-        </main>
-    );
+  return (
+    <main className={styles.main}>
+      <div>Nectworks Migration</div>
+    </main>
+  );
 }
