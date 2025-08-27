c4cf43b0f288aeedbb726b168099c4f178dd0c53
Saumil Diwaker <saumil.diwaker@oracle.com>
2025-08-27 19:07:45 +0530

    fix: update login/logout API endpoints and improve error handling

--- Utils/usePrivateAxios.js
+++ Utils/usePrivateAxios.js
@@ -8,7 +8,7 @@
 */
 
 import { useContext, useEffect } from 'react';
-import { privateAxios, tokenResInterceptor } from '../config/axiosInstance.js';
+import { privateAxios } from '../config/axiosInstance.js';
 import { useRouter } from 'next/navigation';
 import { UserContext } from '../context/User/UserContext';
 
@@ -17,37 +17,8 @@ const usePrivateAxios = () => {
   const { userState } = useContext(UserContext);
   const [user, setUser] = userState;
 
-  // add the interceptor on mount and remove it on unmount
-  useEffect(() => {
-    const responseInterceptor = privateAxios.interceptors.response.use(
-      (response) => response,
-      async (error) => {
-        try {
-          await tokenResInterceptor(error);
-        } catch (error) {
-          /* if the server returned 'unauthorised', navigate user to login
-               not for all errors */
-          if (error.response.status === 401) {
-            // remove user from context
-            setUser(null);
-
-            /* if there was an error in refreshing tokens,
-                navigate the user to login page */
-            router.push('/log-in');
-          } else {
-            /* reject other errors to be handled by the
-                component calling this hook */
-            return Promise.reject(error);
-          }
-        }
-      }
-    );
-
-    return () => {
-      privateAxios.interceptors.response.eject(responseInterceptor);
-    };
-  }, [router, setUser]);
-
+  // The privateAxios instance already has interceptors configured
+  // so we just return it directly
   return privateAxios;
 };
 
